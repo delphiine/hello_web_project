6751279303d452932106d0e2097415b02e044990
delphine <delphine.mubiligi@gmail.com>
2022-07-11 14:30:09 +0100

    building a route challenge

--- app.rb
+++ app.rb
@@ -7,4 +7,16 @@ class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+  end
+
+  get '/submit' do
+    name = params[:name]
+    message = params[:message]
+  
+    return "Thanks #{name}, you sent this message: #{message}"
+  end
 end
